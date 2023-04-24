1e7bf6fe7568cd6da457ed2b07c36eac412d7b28
SaheerabanuParuthikunnan <130434990+SaheerabanuParuthikunnan@users.noreply.github.com>
2023-04-24 14:06:11 +0530

    Create year_wise_creation.sql

---
+++ year_wise_creation.sql
@@ -0,0 +1,25 @@
+SELECT * INTO 
+              year_wise_data
+	FROM  
+       cyclistic_project.dbo.Q1
+
+UNION 
+	
+  SELECT * 
+  
+  FROM 
+        cyclistic_project.dbo.Q2
+	
+  UNION
+  
+	SELECT * 
+  
+  FROM 
+      cyclistic_project.dbo.Q3
+	
+  UNION
+	
+  SELECT * 
+  
+  FROM 
+        cyclistic_project.dbo.Q4
